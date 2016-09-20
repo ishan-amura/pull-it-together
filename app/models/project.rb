e653bcc804ea82db5c03bfdc3c93a90244b0fcda
sonali-amura <sonali@amuratech.com>
2016-09-20 17:39:31 +0530

    Add validation of title description priority to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,15 @@ class Project < ActiveRecord::Base
 	belongs_to :user
 	alias_attribute :creator, :user
 	has_many :project_users
-	has_many :users , through: :project_users
+	has_many :users, through: :project_users
 	alias_attribute :members, :users
 	before_save :add_creator_to_members, if: :user_id_changed?
 	has_many :posts
 	validates_associated :posts
 	validates_associated :tasks
+	validates :title,  presence: true, length: {is: 200}
+	validates :description, allow_blank: true
+	validates :priority, presence: true 
 	def add_creator_to_members
 		self.members << self.creator 
 	end
