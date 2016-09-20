9f2280b6bc6188173ff7bd38575d7a0292e64faa
sonali-amura <sonali@amuratech.com>
2016-09-20 18:10:51 +0530

    Add validations progress

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,7 +11,19 @@ class Project < ActiveRecord::Base
 	validates_associated :tasks
 	validates :title,  presence: true, length: {is: 200}
 	validates :description, allow_blank: true
-	validates :priority, presence: true 
+	validates :priority, presence: true,inclusion: {in: %w(low normal high ASAP)}
+	validates :status, presence: true
+	validates_format_of :status,:with => ^[A-Za-z]+$ 
+	validates :progress, presence: true, numericality: { only_integer: true }, length: {in: 1..3}
+	validate :is_valid_date?
+
+	private
+	  def is_valid_date?
+	    if((due_date.is_a?(Date) rescue ArgumentError) == ArgumentError)
+	      errors.add(:due_date, 'Sorry, Invalid Date.')
+	    end
+	  end
+
 	def add_creator_to_members
 		self.members << self.creator 
 	end
