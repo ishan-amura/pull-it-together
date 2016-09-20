9e8a99d51f75c865bc026536839f3e91268c5699
sonali-amura <sonali@amuratech.com>
2016-09-20 18:39:34 +0530

    Title description timeline validation added

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,8 @@ class Task < ActiveRecord::Base
   belongs_to :user
 
   validates :taskable_type , presence: true, inclusion: { within: %w(Project Task) }
+  validates_datetime :due_date, :after => :started_at 
+  validates :title,  presence: true, length: {is: 200}
+  validates :description, allow_blank: true
   
 end
