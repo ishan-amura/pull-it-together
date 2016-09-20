b5fc0a6adb86329b9b0d19e98f002839da4a115e
sonali-amura <sonali@amuratech.com>
2016-09-20 18:40:17 +0530

    Date status progress validation added

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,5 +7,15 @@ class Task < ActiveRecord::Base
   validates_datetime :due_date, :after => :started_at 
   validates :title,  presence: true, length: {is: 200}
   validates :description, allow_blank: true
-  
+  validates :priority, presence: true,inclusion: {in: %w(low normal high ASAP)}
+  validates :status, presence: true
+  validates_format_of :status,:with => ^[A-Za-z]+$ 
+  validates :progress, presence: true, numericality: { only_integer: true }, length: {in: 1..3}
+  validate :is_valid_date?
+  private
+	def is_valid_date?
+	  if((due_date.is_a?(Date) rescue ArgumentError) == ArgumentError)
+	    errors.add(:due_date, 'Sorry, Invalid Date.')
+	  end
+	end
 end
