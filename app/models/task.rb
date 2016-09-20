fed80fd9a7260208bf2b5787b9276c8e86526843
sonali-amura <sonali@amuratech.com>
2016-09-20 18:45:09 +0530

    Association added in task-post

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :taskable, polymorphic: true
   has_many :tasks, as: :taskable
   belongs_to :user
+  has_many :comments
 
+  validates_associated :posts
   validates :taskable_type , presence: true, inclusion: { within: %w(Project Task) }
   validates_datetime :due_date, :after => :started_at 
   validates :title,  presence: true, length: {is: 200}
