a9e5243f2f00c2be17b2df9eaf7cf65d9d9b7437
sonali-amura <sonali@amuratech.com>
2016-09-20 18:51:30 +0530

    Validations Added title body

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable
 
-  
+  validates_associated :comments
+  validates :title,  presence: true, length: {is: 200}
+  validates :body, allow_blank: true
 end
