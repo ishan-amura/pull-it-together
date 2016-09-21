2090517cfac182cb3171b8ba033266308a3820dc
sonali-amura <sonali@amuratech.com>
2016-09-21 13:31:10 +0530

    Adding test to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:subject) {User.new(name:"ASDAS",email:"as@as.as",password:"password")} 
   
-  it "is invalid without name" do
-   subject.name = "Sonali"
-  expect(subject).to be_valid
+  it "is valid with name" do
+   	subject.name = "Sonali"
+  	expect(subject).to be_valid
   end
-  it "is invalid without email" do
-   subject.email = "name@gmail.com"
-  expect(subject).to be_valid
+  it "is not valid without name" do
+   	subject.name = ""
+  	expect(subject).to_not be_valid
   end
-  
 end
