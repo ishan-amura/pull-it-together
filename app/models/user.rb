1a5ceec01721e2a4d7f53e584541a276e88b1c9b
Ishan Kanade <ishan@amuratech.com>
2016-09-21 18:19:36 +0530

    bugfix for after_initialize callback changed to after_find

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   attr_accessor :initials
-  after_initialize :set_initials
+  after_find :set_initials
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
