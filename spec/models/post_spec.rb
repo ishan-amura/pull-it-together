90afded1ebe49666fb3f15f06a64cdc6aa55a5a2
sonali-amura <sonali@amuratech.com>
2016-09-21 17:15:48 +0530

    rspec test added to post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:subject) { Comment.new(body:"demo bhbh njnjn njnjn", commentable_type: "Post")} 
+  let(:subject) { Post.new(title: "gh kli hjy", body:"demo bhbh njnjn njnjn")} 
   
+  it "is valid with title" do
+   	subject.title = "Gh hnj"
+  	expect(subject).to be_valid
+  end
+  it "is not valid without title" do
+   	subject.title = nil
+  	expect(subject).to_not be_valid
+  end
   it "is valid with body" do
    	subject.body = "PIT bdhh nnjn jdncjn"
   	expect(subject).to be_valid
   end
-  it "is not valid with body" do
-   	subject.body = nil
-  	expect(subject).to_not be_valid
-  end
+  
 end
 
     
