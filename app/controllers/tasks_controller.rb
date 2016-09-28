ab03bba98119e297a12187402ef5a58be4ff25ff
sonali-amura <sonali@amuratech.com>
2016-09-28 19:28:04 +0530

    Show index page done task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
 
   def show
     @project = Project.find(params[:project_id])
-    @task = @project.tasks
+    @task = @project.tasks.find(params[:id])
   end
 
   def new
