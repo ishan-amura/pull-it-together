9d24c5f05d78d951f769e112a9bbbba285b06ac2
sonali-amura <sonali@amuratech.com>
2016-10-01 12:40:56 +0530

    Commit association rspec done

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -16,5 +16,9 @@ RSpec.describe Comment, type: :model do
       assc = described_class.reflect_on_association(:user)
       expect(assc.macro).to eq :belongs_to
     end
+    it " belongs to commentable" do
+      assc = described_class.reflect_on_association(:commentable)
+      expect(assc.macro).to eq :belongs_to
+    end
   end
 end
