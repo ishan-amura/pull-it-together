9c7bf793e612d760dd180b03f7924e7eee142c38
sonali-amura <sonali@amuratech.com>
2016-10-01 12:37:43 +0530

    Commit association rspec done

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Comment, type: :model do
    	subject.body = nil
   	expect(subject).to_not be_valid
   end
+  context "Associations" do
+    it " belongs to user" do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
 end
