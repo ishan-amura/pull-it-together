df399ae009c0352f9c4cfe643b6ae2681baac330
sonali-amura <sonali@amuratech.com>
2016-10-01 12:27:01 +0530

    Task association rspec done

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,6 +16,21 @@ RSpec.describe Post, type: :model do
   	expect(subject).to be_valid
   end
   
+  context "Associations" do
+    it "has many user" do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
+    end
+    it "has many project" do
+      assc = described_class.reflect_on_association(:project)
+      expect(assc.macro).to eq :belongs_to
+    end
+    it "has many comments" do
+      assc = described_class.reflect_on_association(:comments)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+
 end
 
     
