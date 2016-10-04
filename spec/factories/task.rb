46491637b8308924fb739d7855f58cbf4ca738e9
sonali-amura <sonali@amuratech.com>
2016-10-04 18:11:13 +0530

    Factorygirl of task added

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
 factory :project do |f|
   f.title { Faker::Name.name }
-  f.deadline { Faker::Date.forward(days = 1)}
   f.description { Faker::Lorem.paragraph }
   f.progress {Faker::Number.between(0,100)}
+  f.due_date { Faker::Date.forward(days = 1)}
   f.started_at {Faker::Date.forward(0)} 
   f.user_id {Faker::Number.number(2)}
 end
-
 end
