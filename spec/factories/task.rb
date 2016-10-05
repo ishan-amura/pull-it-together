5855af9bf1f44a37efc8b9de4da2a6471b281c3c
sonali-amura <sonali@amuratech.com>
2016-10-05 17:47:07 +0530

    Task model rspec completed using factory girl

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,8 +1,11 @@
+require 'faker'
 FactoryGirl.define do
 factory :task do |f|
   f.title { Faker::Name.name }
   f.description { Faker::Lorem.paragraph }
   f.progress {Faker::Number.between(0,100)}
+  f.status  "active"
+  f.priority "ASAP" 
   f.due_date { Faker::Date.between(Date.today, 1.year.from_now)}
   f.started_at { Date.today} 
   f.taskable_type 'Task'
