ec98664bfe8d2e0711103b11277ddd7048cdb3b1
sonali-amura <sonali@amuratech.com>
2016-10-06 07:32:39 +0530

    post rspec done using factorygirl

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  let(:subject) { Post.new(title: "gh kli hjy", body:"demo bhbh njnjn njnjn")} 
-  
-  it "is valid with title" do
-   	subject.title = "Gh hnj"
-  	expect(subject).to be_valid
+describe Post do
+  context "validation checks " do
+    it "is valid with title" do
+      FactoryGirl.build(:post, title: "gh jk jn").should be_valid
+    end
+    it "is not valid without title" do
+      FactoryGirl.build(:post, title: nil).should_not be_valid
+    end
+    it "is valid with body" do
+      FactoryGirl.build(:post, body: "gh jk jn cdscs").should be_valid
+    end
   end
-  it "is not valid without title" do
-   	subject.title = nil
-  	expect(subject).to_not be_valid
-  end
-  it "is valid with body" do
-   	subject.body = "PIT bdhh nnjn jdncjn"
-  	expect(subject).to be_valid
-  end
-  
   context "Associations" do
     it "has many user" do
       assc = described_class.reflect_on_association(:user)
