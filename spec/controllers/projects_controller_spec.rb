4b0c71ad6f1677fa0822e0ff0a31120b169a450b
sonali-amura <sonali@amuratech.com>
2016-10-12 12:34:32 +0530

    Task observer 34% complete rpsec

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,16 +1,14 @@
   require 'rails_helper'
 
   RSpec.describe ProjectsController, type: :controller do
-     before(:each) do
-     	byebug
+     before(:each) do    
         @user = FactoryGirl.create(:user)
         sign_in @user
         @project = FactoryGirl.attributes_for(:project)
      end
     
     describe "#new" do
-      it "redirects to the show page upon save" do
-      	byebug
+      it "redirects to the show page upon save" do      
          get :new
          expect(response.status).to eq(200)
       end
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -8,24 +8,21 @@ factory :task do |f|
   f.priority  {["low","normal","high","ASAP"][rand(4)]}
   f.due_date { Faker::Date.between(Date.today, 1.year.from_now)}
   f.started_at { Date.today} 
-<<<<<<< HEAD
   f.taskable_type {["Task","Project"][rand(2)]}
   association :user, factory: :user, strategy: :build
 
+  
+  #f.taskable_type {["Task","Project"][rand(2)]} 
   association :taskable, factory: :project, strategy: :build
 
-  factory :task_taskable do
-    association :taskable, factory: :task, strategy: :build
-=======
-  #f.taskable_type {["Task","Project"][rand(2)]}
-  association :user_id, factory: :user, strategy: :build
-  trait :project_task do  
-  	association :taskable, factory: :project, strategy: :build
->>>>>>> 911e4168342f40e798dfd8eed3b2b07d8e62c5ef
-  end
   trait :subtask_task do
 		association :taskable, factory: :task, strategy: :build
   end
+  before(:create) do
+      ActiveRecord::Base.observers.disable :task_observer
+  end
+
+        
   #factory :task_taskable do
   #  association :taskable, factory: :task, strategy: :build
   #end
